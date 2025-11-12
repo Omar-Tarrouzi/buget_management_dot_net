6fd0d194b4afc336ed7327292297c98de99dee83
Mr Tarouzi <omartarrouzicontact@gmail.com>
2025-11-12 17:47:51 +0100

    Ajoutez des fichiers projet.

---
+++ App de gestion de buget version2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace App_de_gestion_de_buget_version2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
